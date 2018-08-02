df1db7c9f57412054c3a27945e18d990287fe8cc
asim altayb <asomy0909@gmail.com>
2018-08-02 21:49:01 +0200

    Create count-char.java

---
+++ count-char.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+
+/**
+ * @author asim altayb 
+ *github/asim-altayb
+ *
+ * Implementation of a character count.
+ * (Slow, could be improved upon, effectively O(n).
+ * */
+
+public class CountChar {
+
+    public static void main(String[] args) {
+  	  	Scanner input = new Scanner(System.in);
+  	  	System.out.print("Enter your text: ");
+      	String str = input.nextLine();
+
+        System.out.println("There are " + CountCharacters(str) + " characters.");
+    }
+	
+    /**
+     * @param str: String to count the characters
+     *
+     * @return int: Number of characters in the passed string
+     * */
+
+    public static int CountCharacters(String str) {
+
+    	int count = 0;
+
+    	if(str == "" || str == null) //Exceptions
+		{ 
+			return 0; 
+		}
+
+        for(int i = 0; i < str.length(); i++) {
+        	if(!Character.isWhitespace(str.charAt(i))) {
+        		count++;
+			}}
+
+        return count;
+     }
+}
